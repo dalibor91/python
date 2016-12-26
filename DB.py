90cf4540a983b0afe4b1ef3087098ca1fdc8c727
dalibor91 <dalibor_menkovic@hotmail.com>
2016-12-26 13:01:12 +0100

    Create DB.py

---
+++ DB.py
@@ -0,0 +1,60 @@
+import MySQLdb
+
+class DB(object):
+
+    def __init__(self):
+
+    	if not hasattr(DB, 'db_user_name'):
+    		raise Exception("db_user_name not provided")
+
+    	if not hasattr(DB, 'db_user_pass'):
+    		raise Exception("db_user_pass not provided")
+
+    	if not hasattr(DB, 'db_host'):
+    		raise Exception("db_host not provided")
+
+    	if not hasattr(DB, 'db_table'):
+    		raise Exception("db_table not provided")
+
+        self.db = MySQLdb.connect(DB.db_host, DB.db_user_name, DB.db_user_pass, DB.db_table)
+
+    @staticmethod
+    def instance():
+        if not hasattr(DB, 'db_instance'):
+            DB.db_intance = DB()
+
+        return DB.db_intance.db;
+
+    @staticmethod
+    def cursor():
+        return DB.instance().cursor();
+
+    @staticmethod
+    def cursor_dictonary():
+        return DB.instance().cursor(MySQLdb.cursors.DictCursor);
+
+    @staticmethod
+    def lastid():
+        return DB.instance().lastrowid;
+
+    @staticmethod
+    def run(str, use_normal_cursor=False):
+        crs = if use_normal_cursor DB.cursor() else DB.cursor_dictonary()
+        crs.execute(str)
+        return crs
+
+    @staticmethod
+    def close():
+        DB.instance().close();
+        del DB.db_intance
+
+
+DB.db_host="localhost"
+DB.db_user_name="user"
+DB.db_user_pass="pass"
+DB.db_table="test"
+
+
+print DB.run("show tables;").fetchall();
+
+DB.close();
